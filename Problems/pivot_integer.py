d726ad28dfd700c612cc1c47e7a9573b9cff71e8
bipsec <bip.sec22@gmail.com>
2024-03-14 09:33:28 +0900

    pivot integer

--- Problems/pivot_integer.py
+++ Problems/pivot_integer.py
@@ -1,5 +1,29 @@
 class Solution:
+
     def pivotInteger(self, n: int) -> int:
+        if n == 1:
+            return 1
+        else:
+            arr = [i for i in range(1, n+1)]
+
+            prefixSum = [0] * (n + 1)
+            suffixSum = [0] * (n + 1)
+            for i in range(1, n + 1):
+                prefixSum[i] = prefixSum[i - 1] + arr[i - 1]
+                suffixSum[n - i] = suffixSum[n - i + 1] + arr[n - i]
+
+            for i in range(1, n):
+                if prefixSum[i] == suffixSum[i+1]:
+                    return i + 1
+            return -1
+
+
+s = Solution()
+print(s.pivotInteger(8))
+print(s.pivotInteger(1))
+print(s.pivotInteger(4))
+print(s.pivotInteger(3))
+print(s.pivotInteger(232))
         
-        for i in range(1, n+1):
-            pass
+        
+
