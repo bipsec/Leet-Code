2bfb400733670e4eb9f5161a6989dc93a5e33853
bipsec <bip.sec22@gmail.com>
2024-03-20 09:02:32 +0900

    cf problems updated

--- Codeforces/pangram.py
+++ Codeforces/pangram.py
@@ -6,28 +6,18 @@ def vula_jaye_na():
     letters = {chr(i+64): i for i in range(1, 27)}
     count = 0
 
-    print(len(set(sentence)), "|-----|", len(letters))
-
     if len(set(sentence)) < len(letters):
         print("NO")
 
     else:
-        print("vailuwr")
-        for key, val in letters:
+        for key, val in letters.items():
             if key in sentence:
                 count += 1
-    print(count)
 
-    if count == 26:
-        print("YES")
-    else:
-        print("NO")
-
-    # if len(set(sentence)) == len(sentence):
-    #     print("YES")
-    # else:
-    #     print("NO")
-
+        if count == 26:
+            print("YES")
+        else:
+            print("NO")
 
 
 vula_jaye_na()
