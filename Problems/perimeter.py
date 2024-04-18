f6527f3d9aa9e83f74f6894bdd6688670a3c426d
bipsec <bip.sec22@gmail.com>
2024-04-18 11:21:10 +0900

    lt problems

---
+++ Problems/perimeter.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandPerimeter(self, grid) -> int:
+
+        R, C = len(grid), len(grid[0])
+        perimeter = 0
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+        return perimeter
+
+
+s = Solution()
+print(s.islandPerimeter([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]]))
+print(s.islandPerimeter([[1]]))
