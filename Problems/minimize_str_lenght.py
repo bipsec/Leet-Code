9e2b7f2a86dda1d3985121327db94e9ccfa8a2ac
bipsec <bip.sec22@gmail.com>
2024-06-14 13:20:55 +0900

    lt-code problem

--- Problems/minimize_str_lenght.py
+++ Problems/minimize_str_lenght.py
@@ -1,3 +1,16 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
         return len(set(s))
+
+
+
+
+
+
+
+
+
+# Dr. Suman Maity
+# Dr. Satish Puri
+# Dr. Patrick Taylor
+# Dr. Huiyuan Yang
