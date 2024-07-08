7ff893c8d768bfc2312b58a7b9c1ea2e5e93340f
bipsec <bip.sec22@gmail.com>
2024-07-08 10:02:47 +0900

    lt-code problem

---
+++ Problems/water_bottles.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+
+        while numBottles >= numExchange:
+            ans += numBottles // numExchange
+            numBottles = numBottles // numExchange + (numBottles % numExchange)
+
+        return ans
+
+
+s = Solution()
+print(s.numWaterBottles(numBottles=9, numExchange=3))
+print(s.numWaterBottles(numBottles=15, numExchange=4))
+print(s.numWaterBottles(numBottles=2, numExchange=2))
