10e6ce57b1dc5c6f1fe8c01245f257b24569cb45
bipsec <bip.sec22@gmail.com>
2024-07-29 13:13:04 +0900

    lt-code problem

---
+++ Problems/bitwise_and_in_range.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while right > left:
+            right &= (right - 1)
+        return right
+
+
+
+
+
+
+
+
+
+
+
+s = Solution()
+print(s.rangeBitwiseAnd(left=5, right=7))
+print(s.rangeBitwiseAnd(left=0, right=0))
+print(s.rangeBitwiseAnd(left=1, right=2147483647))
