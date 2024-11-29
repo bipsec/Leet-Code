6b8aa542b72b592086124dc764c4876cf6d6233b
bipsec <bip.sec22@gmail.com>
2024-11-29 13:51:26 +0900

    lt-code: updated

--- Problems/arr_pairs_are_divisible_by_K.py
+++ Problems/arr_pairs_are_divisible_by_K.py
@@ -1,25 +1,30 @@
+from collections import Counter
+
+
 class Solution:
     def canArrange(self, arr, k: int) -> bool:
-        start, end = 0, len(arr) - 1
-        flag = True
 
-        while start < end:
+        remainder_count = Counter((x % k + k) % k for x in arr)
 
-            a, b = arr[start], arr[end]
-            val = a + b
-            print(val)
-            if val % k != 0:
-                flag = False
-            start += 1
-            end -= 1
+        for rem in list(remainder_count.keys()):
+            if rem == 0:
+                if remainder_count[rem] % 2 != 0:
+                    return False
+            else:
+                if remainder_count[rem] != remainder_count[k - rem]:
+                    return False
 
-        return flag
+        return True
 
 
 # wrong approach -- it's a good problem indeed!
+# remainder niye vaba uchit chilo!
+# not mine
 
 s = Solution()
 # print(s.canArrange(arr=[1, 2, 3, 4, 5, 10, 6, 7, 8, 9], k=5))
 # print(s.canArrange(arr=[1, 2, 3, 4, 5, 6], k=7))
 # print(s.canArrange(arr=[1, 2, 3, 4, 5, 6], k=10))
 print(s.canArrange(arr=[-1, 1, -2, 2, -3, 3, -4, 4], k=3))
+print(s.canArrange(arr=[3, 8, 7, 2], k=10))
+print(s.canArrange(arr=[1, 2], k=3))
