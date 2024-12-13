59fc543ef9df565d53e54df12243354102b6eaed
bipsec <bip.sec22@gmail.com>
2024-12-13 16:02:02 +0900

    lt-code: initialized

---
+++ Problems/account_balance_after_rounder_purchase.py
@@ -0,0 +1,9 @@
+class Solution:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
+        pass
+
+
+s = Solution()
+print(s.accountBalanceAfterPurchase(9))
+print(s.accountBalanceAfterPurchase(15))
+print(s.accountBalanceAfterPurchase(10))
