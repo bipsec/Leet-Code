b7e40d1243b91a0b488a73061142f43d2c5d7e57
bipsec <bip.sec22@gmail.com>
2024-12-19 13:39:17 +0900

    lt-code: solution updated

--- Problems/two_out_of_three.py
+++ Problems/two_out_of_three.py
@@ -19,7 +19,12 @@ class Solution:
         return arr
 
 
+class Solution2:
 
+    def twoOutOfThree(self, nums1, nums2, nums3):
+        set1, set2, set3 = set(nums1), set(nums2), set(nums3)
+        result = (set1 & set2) | (set2 & set3) | (set1 & set3)
+        return list(result)
 
 
 s = Solution()
